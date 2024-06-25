aa59d6d877c115b29d861965798948a264064612
ttsuka <tmak.tsukamoto@gmail.com>
2024-06-26 02:11:32 +0900

    TypeScript環境構築
    npm i -D typescript ts-loader
    
    npx tsc --init
    
    npm install --save-dev @types/terser

--- js/index.ts
+++ js/index.ts
@@ -1,11 +1,13 @@
-import {minify} from 'terser'
+import {minify} from 'terser';
 
 // jsをテキストとして取得
 // github pagesではbase pathが変わることを考慮している TODO: 環境変数にしたい
 const fileUrls = [
   "js/vendor/copyLinkTextHtml.js",
   "js/vendor/copyLinkMarkdown.js",
-  "js/vendor/copyLinkPlaintext.js"].map(path => `${window.location.pathname}${path}`);
+  "js/vendor/copyLinkPlaintext.js"
+].map(path => `${window.location.pathname}${path}`);
+
 Promise.all(fileUrls.map(url =>
   fetch(url).then(response => response.text())
 )).then(async responses => {
@@ -13,17 +15,17 @@ Promise.all(fileUrls.map(url =>
   const fileContents = responses;
 
   const result0 = await minify(fileContents[0], {sourceMap: true});
-  const button0 = document.getElementById('scriptTextHtml');
+  const button0 = document.getElementById('scriptTextHtml') as HTMLAnchorElement;
   button0.href = `javascript:${result0.code}void(0);`;
   button0.classList.remove('hidden');
 
   const result1 = await minify(fileContents[1], {sourceMap: true});
-  const button1 = document.getElementById('scriptMarkdown');
+  const button1 = document.getElementById('scriptMarkdown') as HTMLAnchorElement;
   button1.href = `javascript:${result1.code}void(0);`;
   button1.classList.remove('hidden');
 
   const result2 = await minify(fileContents[2], {sourceMap: true});
-  const button2 = document.getElementById('scriptPlaintext');
+  const button2 = document.getElementById('scriptPlaintext') as HTMLAnchorElement;
   button2.href = `javascript:${result2.code}void(0);`;
   button2.classList.remove('hidden');
 }).catch(error => {
--- webpack.common.js
+++ webpack.common.js
@@ -7,14 +7,24 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
+    filename: './js/[name].js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    // 拡張子を配列で指定
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
 };
